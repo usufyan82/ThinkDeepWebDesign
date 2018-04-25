07f40756f320fb5f79ed03a05366bf564c92e7fa
Umer Sufyan <umer.sufyan@questlab.pk>
2018-04-25 14:29:32 +0500

    changes

--- theme/js/script.js
+++ theme/js/script.js
@@ -521,12 +521,12 @@ $document.ready(function () {
 	 * UI To Top
 	 * @description Enables ToTop Button
 	 */
-	if (isDesktop) {
-		$().UItoTop({
-			easingType: 'easeOutQuart',
-			containerClass: 'ui-to-top icon icon-xs icon-circle icon-darker-filled mdi mdi-chevron-up'
-		});
-	}
+	// if (isDesktop) {
+	// 	$().UItoTop({
+	// 		easingType: 'easeOutQuart',
+	// 		containerClass: 'ui-to-top icon icon-xs icon-circle icon-darker-filled mdi mdi-chevron-up'
+	// 	});
+	// }
 
 	/**
 	 * RD Mailform
